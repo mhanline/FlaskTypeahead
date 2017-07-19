4cb437d271b01a63d3d9639eb14496adfc2dbb1d
Michael Hanline <2013@mikehanline.com>
2017-07-19 14:59:17 +0000

    Initial release

--- typeahead.py
+++ typeahead.py
@@ -26,7 +26,7 @@ def autocomplete():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", form=form)
+    return render_template("index.html",)
 
 if __name__ == '__main__':
     app.run(debug=True)
