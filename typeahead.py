574acdbd8c0ae56eb1fe674e2f7f7c97f1b249cc
Michael Hanline <2013@mikehanline.com>
2017-07-19 14:45:53 +0000

    Removed some lines

--- typeahead.py
+++ typeahead.py
@@ -4,17 +4,13 @@ from wtforms import TextField, Form
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_USER'] = 'readentries'
-app.config['MYSQL_PASSWORD'] = 'G00gl3ro'
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'PRODUCTS'
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_UNIX_SOCKET'] = '/cloudsql/hopeful-20630:us-west1:typeahead-pri'
+app.config['MYSQL_UNIX_SOCKET'] = ''
 
 mysql = MySQL(app)
-
-class SearchForm(Form):
-    autocomp = TextField('Insert City', id='city_autocomplete')
-
 
 @app.route('/_autocomplete', methods=['GET'])
 def autocomplete():
@@ -29,7 +25,6 @@ def autocomplete():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = SearchForm(request.form)
     return render_template("index.html", form=form)
 
 if __name__ == '__main__':
